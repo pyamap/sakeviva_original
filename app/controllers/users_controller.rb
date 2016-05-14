6d76c87bf9488803d20f762ed827d1a61c61f4ea
yukipyamashita <yukiyamashita1125@gmail.com>
2016-05-15 01:32:12 +0900

    Finish Devise CSS

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def destroy
+	end
+
+	def update
+	end
+
+end
