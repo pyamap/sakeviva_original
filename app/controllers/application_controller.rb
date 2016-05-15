c13875ce71ffefb2aa26faadaf83ec28b6f7aad5
yukipyamashita <yukiyamashita1125@gmail.com>
2016-05-15 12:11:26 +0900

    Finish setting admin boolean and add username column setting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  
+
   before_action :authenticate_user!, except: :index #deviseによるログイン精査の為のコード. except: :indexでホーム画面以外は基本的にログインがいる状態にしてある。
+
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  	def configure_permitted_parameters
+  		devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:username, :email, :password, :password_confirmation, :remember_me)}
+  		devise_parameter_sanitizer.for(:sign_up) {|u| u.permit(:login, :username, :email, :password, :remember_me)}
+  		devise_parameter_sanitizer.for(:account_update) {|u| u.permit(:username, :email, :password, :password_confirmation, :current_pasword)}
+  	end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
+	attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :username,
+  	uniqueness: {case_sensitive: :false},
+  	length: {minimum: 4, maximum: 20},
+  	format: { with: /\A[a-z0-9]+\z/, message: "ユーザー名は半角英数字です"}
 end
