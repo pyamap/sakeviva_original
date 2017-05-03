60d9a2b60e8d4259fa1891a6be3760fe740b32f7
yukipyamashita <yukiyamashita1125@gmail.com>
2017-05-03 22:39:31 +0900

    put changes back

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,32 @@
+class UsersController < ApplicationController
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+		@user = User.find(params[:id])
+	end
+
+	def edit
+	end
+
+	def create
+	
+	end
+
+	def destroy
+	end
+
+	def update
+	end
+
+
+	private
+	def user_params
+		params.require(:user).permit(:address,:user_id])
+	end
+
+end
