c245ea2d6e56a1079fcd4a46376d27c5e3278fc9
yukipyamashita <yukiyamashita1125@gmail.com>
2017-07-02 21:53:17 +0900

    Admin setup try9

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,8 +1,8 @@
 class Admin::Base < ApplicationController
 	before_action :authenticate_user! #deviseによるログイン精査の為のコード
-	unless current_user.admin?
-	  redirect_to root_path
-	end
+	#unless current_user.admin?
+	 # redirect_to root_path
+	#end
 	#before_action do
 	#	redirect_to new_user_session_path unless current_user && current_user.admin?
 	#end
