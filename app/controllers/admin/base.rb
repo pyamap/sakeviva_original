b23366ab34eb3ddf7f9687a93a84eeacf862533e
yukipyamashita <yukiyamashita1125@gmail.com>
2017-07-02 21:24:41 +0900

    Admin setup try6

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,5 +1,5 @@
 class Admin::Base < ApplicationController
-	before_action :authenticate_user! #deviseによるログイン精査の為のコード
+ #deviseによるログイン精査の為のコード
 	before_action do
 		redirect_to new_user_session_path unless current_user && current_user.admin?
 	end
