cfe1148409ffa82c799b90f148e0f8a7096f2cce
yukipyamashita <yukiyamashita1125@gmail.com>
2017-07-29 12:43:55 +0900

    fixing double count of shipping fee

--- app/controllers/shippings_controller.rb
+++ app/controllers/shippings_controller.rb
@@ -1,34 +1,36 @@
 class ShippingsController < ApplicationController
 	def shipping_fee
-		unless params[":shop_id"][0].blank? then
-		@shop_id_first = params[":shop_id"][0]
+		unique_shop_ids = params[":shop_id"].uniq
+
+		unless unique_shop_ids[0].blank? then
+		@shop_id_first = unique_shop_ids[0]
 		@shop_id_first.present?
 		@delivery_fee_first = Shipping.find_by("shop_id = ? and prefecture_id = ?", @shop_id_first, params[:prefecture][:prefecture_id]).fee
-		else 
+		else
 		@delivery_fee_first = 0
 		end 
 
-		unless params[":shop_id"][1].blank? then
-		@shop_id_second = params[":shop_id"][1]
+		unless unique_shop_ids[1].blank? then
+		@shop_id_second = unique_shop_ids[1]
 		@shop_id_second.present?
 		@delivery_fee_second = Shipping.find_by("shop_id = ? and prefecture_id = ?", @shop_id_second, params[:prefecture][:prefecture_id]).fee
-		else 
+		else
 		@delivery_fee_second = 0
 		end
 
-		unless params[":shop_id"][2].blank? then
-		@shop_id_third = params[":shop_id"][2]
+		unless unique_shop_ids[2].blank? then
+		@shop_id_third = unique_shop_ids[2]
 		@shop_id_third.present?
 		@delivery_fee_third = Shipping.find_by("shop_id = ? and prefecture_id = ?", @shop_id_second, params[:prefecture][:prefecture_id]).fee
-		else 
+		else
 		@delivery_fee_third = 0
 		end
 
-		unless params[":shop_id"][3].blank? then
-		@shop_id_fourth = params[":shop_id"][3]
+		unless unique_shop_ids[3].blank? then
+		@shop_id_fourth = unique_shop_ids[3]
 		@shop_id_fourth.present?
 		@delivery_fee_fourth = Shipping.find_by("shop_id = ? and prefecture_id = ?", @shop_id_second, params[:prefecture][:prefecture_id]).fee
-		else 
+		else
 		@delivery_fee_fourth = 0
 		end
 
