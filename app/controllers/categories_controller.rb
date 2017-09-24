c6cb3ae073c17b3d1b874033425af04fa9ec2325
yukipyamashita <yukiyamashita1125@gmail.com>
2017-09-24 21:20:07 +0900

    Add the order for products of each category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,19 +1,19 @@
 class CategoriesController < ApplicationController
 
 	def category_sake
-		@sake_products = Product.all.where(category_id: 1)
+		@sake_products = Product.all.where(category_id: 1).order(:id)
 	end
 
 	def category_grocery
-		@grocery_products = Product.all.where(category_id: 2)
+		@grocery_products = Product.all.where(category_id: 2).order(:id)
 	end
 
 	def category_food
-		@food_products = Product.all.where(category_id: 3)
+		@food_products = Product.all.where(category_id: 3).order(:id)
 	end
 
 	def category_craft
-		@craft_products = Product.all.where(category_id: 4)
+		@craft_products = Product.all.where(category_id: 4).order(:id)
 	end
 
 	def category_first
